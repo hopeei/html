c71550be4da7aca81cc34f56db4da0cf91481a52
hopeei <gopeydanylo@gmail.com>
2023-06-16 15:31:49 +0300

    intro js

---
+++ js/intro.js
@@ -0,0 +1,48 @@
+// task 2
+alert("Hopei")
+// task 3
+let a = 5;
+let b = 3;
+alert(a,b);
+a = b = 5;
+alert(a,b)
+// task 4
+const myObject = {
+    myString: "Hello JS!",
+    myNumber: 35,
+    myBoolean: true,
+    myUndefined: undefined,
+    myNull: null
+  };
+  console.log(myObject);
+  // task 5
+  const isAdult = confirm("You are 18+?");
+  console.log(isAdult);
+  // task 6
+  const name = 'Danylo';
+  const surname = 'Hopei';
+  const group = 'T4';
+  const birthyear = 2004;
+  const married = false;
+  console.log(`Birthyear: ${birthyear}, type: ${typeof (birthyear)}`);
+  console.log(`Married: ${married}, type: ${typeof (married)}`);
+  console.log(`Name: ${name}, type: ${typeof (name)}`);
+  console.log(`Surname: ${surname}, type: ${typeof (surname)}`);
+  console.log(`Group: ${group}, type: ${typeof (group)}`);
+  const menu = null;
+  const mobile = undefined;
+  console.log(`Menu: ${menu}, type: ${typeof (menu)}`); 
+  console.log(`Mobile: ${mobile}, type: ${typeof (mobile)}`);
+  // task 7
+  let login = prompt ("Please enter your login: ");
+  let email = prompt("Please enter your email: ");
+  let password = prompt ("Please enter your password: ");
+  alert(`Your login ${login}, your email ${email}, your password ${password}`);
+  // task 8
+const secondsInHour = 60 * 60;
+const secondsInDay = secondsInHour * 24;
+const secondsInMonth = secondsInDay * 30;
+aler(`The number of seconds in an hour: ${secondsInHour}`);
+aler(`The number of seconds in a day: ${secondsInDay}`);
+alert(`The number of seconds in a month: ${secondsInMonth}`);
+  
